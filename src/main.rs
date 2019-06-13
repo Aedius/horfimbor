c63bf12d7e74eb53408f6d14046471147bf03886
Aedius <aedius.filmania@gmail.com>
2019-06-13 15:19:14 +0200

    add useless test

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 use std::{thread, time};
 
+
 fn main() {
 
     let time = time::Duration::from_secs(1);
     let time2 = time::Duration::from_secs(10);
 
     loop {
-        println!("Hello, worlds !");
+        println!("Hello, {} !", get_world_name());
         thread::sleep(time);
-        println!("good by, worlds !");
+        println!("good by, {} !", get_world_name());
         thread::sleep(time2);
+    }
+}
+
+pub fn get_world_name() -> &'static str {
+    "World"
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_get_world_name() {
+        assert_eq!(get_world_name(), "World");
     }
 }
