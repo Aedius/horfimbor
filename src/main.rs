1da108dc8d59b6005fa0aa04bfda8f3925be4b58
Aedius <aedius.filmania@gmail.com>
2019-06-13 20:55:10 +0200

    web server

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
-use std::{thread, time};
+extern crate hyper;
+
+use hyper::{Body, Request, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+
+const PHRASE: &str = "Hello, World!";
+
+fn hello_world(_req: Request<Body>) -> Response<Body> {
+    println!("call");
+    Response::new(Body::from(PHRASE))
+}
+
 
 fn main() {
-    let time = time::Duration::from_secs(1);
 
-    loop {
-        println!("Hello, World ! I'm lignée server !");
-        thread::sleep(time);
-    }
+    println!("START ok");
+
+    // This is our socket address...
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+// A `Service` is needed for every connection, so this
+// creates one from our `hello_world` function.
+    let new_svc = || {
+        // service_fn_ok converts our function into a `Service`
+        service_fn_ok(hello_world)
+    };
+
+    let server = Server::bind(&addr)
+        .serve(new_svc)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+// Run this server for... forever!
+    hyper::rt::run(server);
+
 }
 
 pub fn get_world_name() -> &'static str {
