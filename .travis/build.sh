66c661d4a84a2b56bfb05d42beb59f79953cb567
Aedius <aedius.filmania@gmail.com>
2019-06-15 15:35:08 +0200

    an other travis test

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,7 +2,7 @@
 
 if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then
     echo "$PROJECT is being built"
-    cd $TRAVIS_BUILD_DIR/$PROJET
+    cd $TRAVIS_BUILD_DIR/$PROJECT
     cargo build --verbose --all
 else
     echo "$PROJECT is NOT being built"
--- .travis/test.sh
+++ .travis/test.sh
@@ -3,7 +3,7 @@
 
 if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then
     echo "$PROJECT is being tested"
-    cd $PROJET
+    cd $TRAVIS_BUILD_DIR/$PROJECT
     cargo test --verbose --all
 else
     echo "$PROJECT is NOT being tested"
