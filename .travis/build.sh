716f31c35072928c2f48ac4e29df437fd9b76a5b
Aedius <aedius.filmania@gmail.com>
2019-06-16 15:22:08 +0200

    build in wasm

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+FOO=${IS_WASM:=false}
+
 if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then
     cd $TRAVIS_BUILD_DIR/$PROJECT
+
     echo "$PROJECT is being built"
-    cargo build --verbose --all
+
+    if [[ "$IS_WASM" = true ]] ; then
+        cargo build  --target wasm32-unknown-unknown --verbose --all
+    else
+        cargo build --verbose --all
+    fi
+
     echo "$PROJECT is being tested"
-    cargo test --verbose --all
+
+    if [[ "$IS_WASM" = true ]] ; then
+        cargo test --target wasm32-unknown-unknown --verbose --all
+    else
+        cargo test --verbose --all
+    fi
+
     echo "$PROJECT is done"
+
 else
     echo "$PROJECT is NOT being built"
 fi
