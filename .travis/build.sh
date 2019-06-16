bddbbcbc45b051d4ed64befb68c04364ee871166
Aedius <aedius.filmania@gmail.com>
2019-06-16 13:00:34 +0200

    only one build by project

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then
-    echo "$PROJECT is being built"
     cd $TRAVIS_BUILD_DIR/$PROJECT
+    echo "$PROJECT is being built"
     cargo build --verbose --all
+    echo "$PROJECT is being tested"
+    cargo test --verbose --all
+    echo "$PROJECT is done"
 else
     echo "$PROJECT is NOT being built"
 fi
